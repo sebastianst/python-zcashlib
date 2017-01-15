251876f4ba17c67cd566a3c21c3c1ea9f549e98e
Sebastian Stammler <stammler.s@gmail.com>
2017-01-15 23:23:14 +0100

    Use Getters for N and K

--- core/serialize.py
+++ core/serialize.py
@@ -9,15 +9,15 @@
 # propagated, or distributed except according to the terms contained in the
 # LICENSE file.
 
-from zcash.core import coreparams as coreparams
+from zcash.core import *
 import struct
 from bitstring import pack
 
 def bytes_from_solution(sol):
     # A solution integer array contains 2^k/8 fields of values with max n/(k+1)+1 bits
-    n, k = coreparams.N, coreparams.K
+    n, k = GetN(), GetK()
     size = n//(k+1) + 1
-    count = 2**(coreparams.K)
+    count = 2**k
     assert len(sol) == count, \
             "Solution array contains wrong number of integers."
 
